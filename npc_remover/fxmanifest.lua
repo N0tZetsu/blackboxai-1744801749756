44f3b7611dab9acc8499cbc6750a884a2a9c40c2
N0tZetsu <emamainn@gmail.com>
2025-04-16 08:09:15 -0300

    Add npc_remover/fxmanifest.lua 1744801755193

---
+++ npc_remover/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BLACKBOXAI'
+description 'A resource to remove NPCs from the game world'
+version '1.0.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css'
+}
