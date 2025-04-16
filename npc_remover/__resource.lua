120f67913578e6e96d86b8e0800d0503cb97e339
N0tZetsu <emamainn@gmail.com>
2025-04-16 08:13:42 -0300

    Add npc_remover/__resource.lua 1744802022538

---
+++ npc_remover/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+game 'gta5'
+
+author 'BLACKBOXAI'
+description 'A resource to remove NPCs from the game world'
+version '1.0.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css'
+}
